fe7ed33f5fc22c28596e0d72cb0d9eee382938f7
Jaime Hellman <jaime_hellman@hotmail.com>
2021-11-25 23:43:26 -0300

    update

---
+++ publico/js/script.js
@@ -0,0 +1,10 @@
+
+const nomeColaborador = document.getElementById('usuario');
+const nomeFoto = document.getElementById('nomeFoto');
+const localFoto = document.getElementById('localFoto');
+const dataFoto = document.getElementById('dataFoto');
+const foto = document.getElementById('foto');
+
+function handleFile(e) {
+  e.preventDefault();
+}
