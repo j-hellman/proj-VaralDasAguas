97dbfb7c89c868cc9a33b65714a01ab4a6f0c054
Jaime Hellman <jaime_hellman@hotmail.com>
2021-12-06 10:17:05 -0300

    function handleFile

--- publico/js/script.js
+++ publico/js/script.js
@@ -1,18 +1,18 @@
+const nomeColaborador = document.getElementById('tNome');
+const nomeFoto = document.getElementById('tNomeFoto');
+const localFoto = document.getElementById('tLocal');
+const dataFoto = document.getElementById('tData');
+const foto = document.getElementById('tFoto');
 
 function handleFile() {
-  const nomeColaborador = document.getElementById('tNome');
-  const nomeFoto = document.getElementById('tNomeFoto');
-  const localFoto = document.getElementById('tLocal');
-  const dataFoto = document.getElementById('tData');
-  const foto = document.getElementById('tFoto');
 
-  const lista = [{
-    NomeColaborador: nomeColaborador,
-    NomeFoto: nomeFoto,
-    LocalFoto: localFoto,
-    DataFoto: dataFoto,
-    Foto: foto
-  }];
+  const lista = {
+    NomeColaborador: (nomeColaborador.value),
+    NomeFoto: (nomeFoto.value),
+    LocalFoto: (localFoto.value)
+    // DataFoto: (dataFoto.value),
+    // Foto: (foto.value)
+  };
 
   console.log(lista);
 }
